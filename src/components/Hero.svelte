d2f0f01404cef0ac01788567dbb86604a2d0644d
Sammy Hajhamid <sh@pepsi.pw>
2024-04-06 22:23:34 -0700

    scroll

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,9 +1,26 @@
 <script>
+  import { onMount } from "svelte";
   import HeroDetails from "./HeroDetails.svelte";
   import HeroTitle from "./HeroTitle.svelte";
+
+  let scale = 1;
+
+  onMount(() => {
+    const updateScale = () => {
+      const scrollVal = window.scrollY;
+      const newScale = Math.max(60, 100 - (scrollVal * 30) / 1500) / 100;
+      scale = newScale;
+    };
+
+    window.addEventListener("scroll", updateScale);
+
+    return () => {
+      window.removeEventListener("scroll", updateScale);
+    };
+  });
 </script>
 
-<div class="contents" id="scaleOnScroll">
+<div class="contents" id="scaleOnScroll" style="transform: scale({scale});">
   <HeroTitle />
   <HeroDetails />
 </div>
@@ -11,7 +28,7 @@
 <style>
   .contents {
     z-index: 0;
-    top: 45%;
+    top: 30%;
     left: 20%;
     transform: translate(0%, -50%);
     max-width: 60%;
