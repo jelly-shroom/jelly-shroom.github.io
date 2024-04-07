6ddc0445b055b01b511964d629b22259f11b87e3
jelly <kihyvr@gmail.com>
2024-04-06 20:51:27 -0700

    Add fonts.css import to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,10 @@
   import Navbar from "../components/Navbar.svelte";
   import Hero from "../components/Hero.svelte";
   import Blobs from "../components/Blobs.svelte";
-  
+
   import "../styles/general.css";
   import "../styles/home.css";
+  import "../styles/fonts.css";
 </script>
 
 <Navbar />
