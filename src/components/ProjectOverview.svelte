2bec4a421a859ad43a8caed69f12a47e6d66fa05
jelly <kihyvr@gmail.com>
2024-04-07 01:04:21 -0700

    sdate

---
+++ src/components/ProjectOverview.svelte
@@ -0,0 +1,59 @@
+
+<div id="caseStudyBanner">
+    <img src="resources/images/covers/eDrops.jpg" id="coverImage_eDrops">
+</div>
+<div class="caseContainer-image">
+    <div class="title">
+        <h1><a href="https://community.edroplets.org/" class="externalLink" target="_blank">eDrops Community</a> is a platform dedicated to 
+            building a collaborative hub to inspire electrowetting on dielectric (EWOD) research and development.
+        </h1>
+        <h3>2022</h3>
+    </div>
+    <div class="columnContainer">
+        <div class="headColumn">
+            <h2>Overview</h2>
+        </div>
+        <div class='bodyColumn'>
+            <p>
+                During my internship at UCLA's micro and nano manufacturing lab, I was tasked with designing a website centered around building a <strong>community</strong> 
+                for digital microfluidics enthusiasts, welcoming both beginners and experts in the field. The goal was to create a platform that would 
+                inspire <strong>collaboration and innovation</strong> in the field of EWOD research and development.
+            </p>
+            <br>
+            <p>
+                eDrops Community is an <strong>open-source platform</strong> that provides a space for anyone interested in 
+                microfluidics to come together and share their projects and ideas. 
+            </p>
+            <br>
+            <p>
+                Outside of designing the website, I was also tasked with creating creative videos showcasing the capabilities of UCLA's fabricated EWOD chips. 
+                Working with PhD students allowed me to gain firsthand experience in the research process and expose myself to the world of academia.
+            </p>
+
+            <div class="tagContainer">
+                <div class="tags">internship @UCLA</div>
+                <div class="tags">duration - 1 month</div>
+                <div class="tags">team size - 3</div>
+                <div class="tags">tools - figma, procreate</div>
+                <div class="tags">role - ui designer, ux designer</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    #caseStudyBanner {
+    width: 100%;
+    height: calc(50vh - 125px);
+}
+
+    #caseStudyBanner img {
+    width: 100%;
+    height: 50vh;
+    object-fit: cover;
+   
+
+    position:absolute;
+    top: 0;
+}
+</style>
