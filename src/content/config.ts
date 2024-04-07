7ecf770ea7e9e077c56b2f4b899d4086e7ab6e43
Sammy Hajhamid <sh@pepsi.pw>
2024-04-07 01:00:43 -0700

    asdasd

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
 import { z, defineCollection } from "astro:content";
 
-const projectCollections = defineCollection({
+const projects = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()),
-    image: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+      image: image(),
+      alt: z.string(),
+    }),
 });
 
 export const collections = {
-  projects: projectCollections,
+  projects,
 };
