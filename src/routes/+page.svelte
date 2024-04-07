0d1fa4fee110442d6277f38b18bc06a29f380b8c
jelly <kihyvr@gmail.com>
2024-04-06 20:44:59 -0700

    Add general and home styles to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
   import Navbar from "../components/Navbar.svelte";
   import Hero from "../components/Hero.svelte";
   import Blobs from "../components/Blobs.svelte";
+  
+  import "../styles/general.css";
+  import "../styles/home.css";
 </script>
 
 <Navbar />
