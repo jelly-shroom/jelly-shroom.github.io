289938944eda3506a1111322eefc5446ffa8897f
Sammy Hajhamid <sh@pepsi.pw>
2024-04-06 20:41:00 -0700

    fix blob!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import Navbar from '../components/Navbar.svelte';
-    import Hero from '../components/Hero.svelte';
-    import Blobs from '../components/Blobs.svelte';
+  import Navbar from "../components/Navbar.svelte";
+  import Hero from "../components/Hero.svelte";
+  import Blobs from "../components/Blobs.svelte";
 </script>
 
 <Navbar />
