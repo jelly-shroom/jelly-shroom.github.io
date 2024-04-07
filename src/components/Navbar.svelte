bda3d93f5205832f0ac8df442cbc4f783944decc
Sammy Hajhamid <sh@pepsi.pw>
2024-04-06 20:13:22 -0700

    ijwefiojsd

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<div class="navbar">
+  <div class="home-link">
+    <a href="https://jessicasheng.me/home">Jessica Sheng</a>
+  </div>
+
+  <div class="page-links">
+    <a href="jessicasheng-portfolio.html" class="hover-underline-animation"
+      >project dump</a
+    >
+    <a href="jessicasheng-aboutMe.html" class="hover-underline-animation"
+      >about me</a
+    >
+  </div>
+</div>
