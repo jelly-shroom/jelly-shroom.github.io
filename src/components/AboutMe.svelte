5b78a818a1ebbde7d6b7db510ded5d55f3bc0fd6
Sammy Hajhamid <sh@pepsi.pw>
2024-04-07 14:17:21 -0700

    about me content

---
+++ src/components/AboutMe.svelte
@@ -0,0 +1,56 @@
+<script>
+  import ProfilePicture from "../images/aboutMe/aboutMepfp.jpg";
+  import Scrollable from "./hero/Scrollable.svelte";
+</script>
+
+<Scrollable>
+  <div class="aboutMeColumns">
+    <div class="pfp">
+      <img src={ProfilePicture.src} width="200" alt="jessica sheng" />
+    </div>
+
+    <div class="description">
+      <h1>i’m jessica sheng, a designer based in la.</h1>
+      <p>
+        I am currently a student of the <strong>Iovine and Young Academy</strong
+        >{" "}
+        at the University of Southern California, and I am pursuing a degree in{" "}
+        <strong>Arts, Technology, and the Business of Innovation</strong>. I
+        love tinkering with new technologies and testing out new solutions to
+        problems, and I am always looking for new ways to apply my skills to
+        create meaningful experiences for others. Thanks for stopping by!
+      </p>
+    </div>
+  </div>
+</Scrollable>
+
+<style>
+  strong {
+    color: var(--darkAccent);
+  }
+  h1 {
+    font-family: "Kodchasan";
+    line-height: 1.5;
+    font-size: 2em;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-family: "Quicksand";
+    line-height: 1.5;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 100%;
+  }
+
+  .aboutMeColumns {
+    display: flex;
+    justify-content: space-between;
+    gap: 5%;
+    align-items: center;
+  }
+</style>
---
+++ src/components/hero/Scrollable.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { onMount } from "svelte";
+  import HeroDetails from "./HeroDetails.svelte";
+  import HeroTitle from "./HeroTitle.svelte";
+
+  let scale = 1;
+
+  onMount(() => {
+    const updateScale = () => {
+      const scrollVal = window.scrollY;
+      const newScale = Math.max(60, 100 - (scrollVal * 30) / 1500) / 100;
+      scale = newScale;
+    };
+
+    window.addEventListener("scroll", updateScale);
+
+    return () => {
+      window.removeEventListener("scroll", updateScale);
+    };
+  });
+</script>
+
+<div class="contents" id="scaleOnScroll" style="transform: scale({scale});">
+  <slot />
+</div>
+
+<style>
+  .contents {
+    z-index: 0;
+    top: 30%;
+    left: 20%;
+    transform: translate(0%, -50%);
+    max-width: 60%;
+
+    position: fixed;
+  }
+
+  #scaleOnScroll {
+    z-index: 0;
+  }
+
+  @media screen and (max-width: 400px) {
+    .contents {
+      max-width: 90%;
+      left: 15%;
+    }
+  }
+</style>
