679a808a6896081986be8fae08aa248cb789395b
Sammy Hajhamid <sh@pepsi.pw>
2024-04-06 21:07:44 -0700

    Update fonts and styles

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,30 +1,19 @@
+<script>
+  import HeroTitle from "./HeroTitle.svelte";
+</script>
+
 <div class="contents" id="scaleOnScroll">
-    <div id="main-head-wrapper">
-        <h1>hi! i'm jessica, and i design</h1>
+  <HeroTitle />
+  <div class="bottomContentWrapper">
+    <h2>welcome to my creative hub :)</h2>
 
-        <div class="text"></div>
-        <!-- SVG GOOEY FILTER-->
-        <svg>
-            <filter id="gooey">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="9" result="blur"></feGaussianBlur>
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -8"
-                    result="gooey"></feColorMatrix>
-                <feComposite in="SourceGraphic" in2="gooey" operator="atop"></feComposite>
-            </filter>
-        </svg>
-    </div>
+    <ul>
+      <li>spatial design</li>
+      <li>product design</li>
+      <li>ui design</li>
+      <li>...and more!</li>
+    </ul>
 
-
-    <div class="bottomContentWrapper">
-        <h2>welcome to my creative hub :)</h2>
-
-        <ul>
-            <li>spatial design</li>
-            <li>product design</li>
-            <li>ui design</li>
-            <li>...and more!</li>
-        </ul>
-
-        <a class='button' href=placeholder-nav-link.html>donwload resume</a>
-    </div>
+    <a class="button" href="placeholder-nav-link.html">donwload resume</a>
+  </div>
 </div>
---
+++ src/components/HeroTitle.svelte
@@ -0,0 +1,47 @@
+<div>
+  <h1>hi! i'm jessica, and i design</h1>
+
+  <div class="text"></div>
+  <!-- SVG GOOEY FILTER-->
+  <svg>
+    <filter id="gooey">
+      <feGaussianBlur in="SourceGraphic" stdDeviation="9" result="blur"
+      ></feGaussianBlur>
+      <feColorMatrix
+        in="blur"
+        mode="matrix"
+        values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -8"
+        result="gooey"
+      ></feColorMatrix>
+      <feComposite in="SourceGraphic" in2="gooey" operator="atop"></feComposite>
+    </filter>
+  </svg>
+</div>
+
+<style>
+  div {
+    height: 90%;
+    width: 100%;
+
+    font-family: "Kodchasan", sans-serif;
+  }
+
+  h1 {
+    font-family: "Kodchasan";
+    font-size: 3em;
+  }
+
+  @media screen and (max-width: 1620px) {
+    div {
+      flex-direction: column !important;
+      align-items: left !important;
+      justify-content: left !important;
+
+      gap: 1.75em !important;
+    }
+
+    div svg {
+      height: 50;
+    }
+  }
+</style>
