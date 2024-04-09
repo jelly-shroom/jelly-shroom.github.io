fe4b6af908b21020775fb171b58617d169c8f1ff
Sammy Hajhamid <sh@pepsi.pw>
2024-04-09 01:19:38 -0700

    asdasd

--- src/components/PhotoGallery.svelte
+++ src/components/PhotoGallery.svelte
@@ -3,23 +3,23 @@
   const gridTemplate = galleryLayout.map((area) => `"${area}"`).join(" ");
 </script>
 
-<div
-  style={`margin: auto;
-            width: 90%;
-            display: grid;
-
-            gap: 20px;
-            margin-top: -20px;
-            margin-bottom: 20px;
-            grid-template-areas: ${gridTemplate};`}
->
+<div class="fwa" style={`grid-template-areas: ${gridTemplate};`}>
   <slot />
-</div>
+  <style>
+    .fwa {
+      margin: auto;
+      width: 90%;
+      display: grid;
 
-<style>
-  div :global(div) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-</style>
+      gap: 20px;
+      margin-top: -20px;
+      margin-bottom: 20px;
+    }
+
+    div :global(div) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  </style>
+</div>
