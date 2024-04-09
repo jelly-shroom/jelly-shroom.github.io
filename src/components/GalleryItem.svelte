b89cacbf835b8fe34b4d492716a76e91a642f6e8
Sammy Hajhamid <sh@pepsi.pw>
2024-04-08 21:57:21 -0700

    adasdas

---
+++ src/components/GalleryItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let image: any;
+  export let alt: string;
+  export let caption: string;
+</script>
+
+<div>
+  <img src={image.src} {alt} />
+  <p>{caption}</p>
+</div>
--- src/components/PhotoGallery.svelte
+++ src/components/PhotoGallery.svelte
@@ -1,64 +1,15 @@
-<script lang="ts">
-  export let imgSrc: string[];
-  export let alt: string[];
-  export let imgCaption: string[];
-
-  export let uniqueID: string[] = imgSrc.map((_, index) => `img${index}`);
-</script>
-
 <div>
-  {#each imgSrc as image, index}
-    <div id={uniqueID[index]} class="galleryItem">
-      <img src={image} alt={alt[index]} />
-      <p>{imgCaption[index]}</p>
-    </div>
-  {/each}
+  <slot />
 </div>
 
 <style>
   div {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: auto;
-    gap: 20px;
-    margin-top: -20px;
-    margin-bottom: 20px;
-  }
-
-  div img {
-    width: 100%;
-    height: 100%;
-    margin-bottom: 10px;
-    overflow: hidden;
-
-    object-fit: cover;
-  }
-
-  .galleryItem {
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+    flex-direction: row;
   }
-
-  .galleryItem p {
-    font-size: 1em;
-    margin-top: -20px;
-    text-align: center;
-  }
-
-  #marginBottom {
-    margin-bottom: 30px;
-  }
-
-  #verticalImgLeft {
-    grid-area: 2/ 1 / span 2/ 1;
-  }
-
-  #verticalImgRight {
-    grid-area: 2/ 3 / span 2 /3;
-  }
-  #HACKING {
-    grid-area: 4/ 1/ 4 / span 3;
-    margin-top: -10px;
+  div > * {
+    margin: 0.5rem;
   }
 </style>
