f945190b923639a8521f191b588fcff0218d663e
jelly <kihyvr@gmail.com>
2024-04-08 23:42:20 -0700

    gallery

--- src/components/GalleryItem.svelte
+++ src/components/GalleryItem.svelte
@@ -2,9 +2,26 @@
   export let image: any;
   export let alt: string;
   export let caption: string;
+  export let gridArea: string;
 </script>
 
-<div>
+<div style="grid-area: {gridArea};">
   <img src={image.src} {alt} />
   <p>{caption}</p>
 </div>
+
+<style>
+  div {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1 1 30%;
+  }
+  p {
+    font-size: 1em;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+</style>
--- src/components/PhotoGallery.svelte
+++ src/components/PhotoGallery.svelte
@@ -1,15 +1,46 @@
-<div>
+<script lang="ts">
+  export let galleryType: string;
+</script>
+
+<div class={galleryType}>
   <slot />
 </div>
 
 <style>
-  div {
+  .column {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    flex-direction: row;
+    flex-direction: column;
+    align-items: center;
   }
-  div > * {
-    margin: 0.5rem;
+  .drTrustGallery {
+    margin: auto;
+    width: 90%;
+    display: grid;
+    grid-template-areas:
+      "a b c"
+      "d e f"
+      "d g f"
+      "h h h";
+
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    margin-top: -20px;
+    margin-bottom: 20px;
+  }
+
+  div :global(img) {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin-bottom: 10px;
+    overflow: hidden;
+
+    object-fit: cover;
+  }
+
+  div :global(div) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
