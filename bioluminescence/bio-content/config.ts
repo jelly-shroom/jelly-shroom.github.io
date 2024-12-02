eb9950016cd501e4a56d51dff5de08e76e8e4306
jellyshroom <kihyvr@gmail.com>
2024-12-01 19:35:50 -0800

    Revert "rm duplicates"
    This reverts commit 9dca1f84e7f9132fe648fa1c45d5456ef41e1298.

---
+++ bioluminescence/bio-content/config.ts
@@ -0,0 +1,38 @@
+import { z, defineCollection } from "astro:content";
+
+const projects = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+      image: image(),
+      alt: z.string(),
+      date: z.coerce.date(),
+    }),
+});
+
+const personals = defineCollection({
+  type: "content",
+  schema: z.object({
+    label: z.string(),
+    order: z.number(),
+  }),
+});
+
+const journals = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    alt: z.string(),
+    date: z.coerce.date(),
+  }),
+});
+
+export const collections = {
+  projects,
+  personals,
+  journals,
+};
