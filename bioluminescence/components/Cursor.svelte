c17250bd003a916d4d7963494c0dbc7a6e6e9e61
jellyshroom <kihyvr@gmail.com>
2024-12-01 20:01:42 -0800

    phone scale issue

--- bioluminescence/components/Cursor.svelte
+++ bioluminescence/components/Cursor.svelte
@@ -167,7 +167,7 @@
   });
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="800">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
   <defs>
     <filter id="goo">
       <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
